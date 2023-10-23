74a754a3033b57b359695f092cc83c784f71e7d3
Maksi333 <Simon_n_andersen@live.dk>
2023-10-23 10:43:38 +0200

    void instead boolean anvendDosis

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -12,9 +12,8 @@ public class PN {
     /**
      * Registrer datoen for en anvendt dosis.
      */
-    public boolean anvendDosis(LocalDate dato) {
+    public void anvendDosis(LocalDate dato) {
 
-        return false;
     }
 
     /** Returner antal gange ordinationen er anvendt. */
