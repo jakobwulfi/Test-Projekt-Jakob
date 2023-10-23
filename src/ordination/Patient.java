7eb0575e02d88c25ae3ed46423fc1c895c0de02f
Maksi333 <Simon_n_andersen@live.dk>
2023-10-23 09:48:31 +0200

    arraylist ordinationer

--- src/ordination/Patient.java
+++ src/ordination/Patient.java
@@ -1,9 +1,13 @@
 package ordination;
 
+import java.util.ArrayList;
+
 public class Patient {
     private final String cprNr;
     private final String navn;
     private final double vægt;
+    private final ArrayList<Ordination> ordiantioner = new ArrayList<>();
+
 
     public Patient(String cprNr, String navn, double vægt) {
         this.cprNr = cprNr;
