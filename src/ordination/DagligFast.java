e236aa85f18e28e913abfb64f3d6cc0d13e9dfb3
Jaqass <jacobsorensen524@gmail.com>
2023-10-23 11:24:56 +0200

    Daglig fast

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,35 @@
 package ordination;
 
-public class DagligFast {
+import java.time.LocalDate;
+import java.util.Arrays;
 
+public class DagligFast extends  Ordination {
+
+    private final Dosis[] doser;
+
+
+
+
+    protected DagligFast(LocalDate startDato, LocalDate slutDato, Dosis[] doser) {
+        super(startDato, slutDato);
+        this.doser = doser;
+    }
+
+    @Override
+    public double døgnDosis() {
+        double døDosis = 0;
+        for (Dosis dos : doser){
+            døDosis += dos.getAntal();
+        }
+        return døDosis;
+    }
+
+    @Override
+    public double samletDosis() {
+        return døgnDosis() * antalDage();
+    }
+    @Override
+    public String getType() {
+        return "Daglig Fast";
+    }
 }
