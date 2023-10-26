0a93b5c01c5e23fde3475cc804b4b0e6aa81c75a
Jaqass <jacobsorensen524@gmail.com>
2023-10-26 11:52:40 +0200

    bruh

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -25,14 +25,30 @@ class ControllerTest {
         Patient patient =  Controller.opretPatient("121256-0512", "Jane Jensen", 63.4);
         Lægemiddel lægemiddel = Controller.opretLægemiddel("Paracetamol", 1, 1.5,
                 2, "Ml");
+        LocalDate start = LocalDate.of(2023,9,7);
+        LocalDate slut = LocalDate.of(2023,9,12);
+        double morgenAntal = 0;
+        double middagAntal = 1;
+        double aftenAntal = 2;
+        double natAntal = 3;
 
-        DagligFast dagligFast1 = Controller.opretDagligFastOrdination(LocalDate.of(2023,9,7),
-                LocalDate.of(2023,9,12),patient,lægemiddel,
-                0,1,2,3);
+        DagligFast dagligFast1 = Controller.opretDagligFastOrdination(start, slut,patient,lægemiddel,
+                morgenAntal,middagAntal,aftenAntal,natAntal);
 
-        DagligFast dagligFastTest1 = (DagligFast) Controller.getAllPatienter().get(0).getOrdiantioner().get(0);
         //Test 1 oprettes der et objekt af klassen DagligFast
-        assertSame(dagligFast1, dagligFastTest1);
+        assertNotNull(dagligFast1);
+        assertSame(lægemiddel,dagligFast1.getLægemiddel());
+        assertSame(start,dagligFast1.getStartDato());
+        assertSame(slut,dagligFast1.getSlutDato());
+        assertEquals(morgenAntal,dagligFast1.getDoser()[0].getAntal());
+        assertEquals(middagAntal,dagligFast1.getDoser()[1].getAntal());
+        assertEquals(aftenAntal,dagligFast1.getDoser()[2].getAntal());
+        assertEquals(natAntal,dagligFast1.getDoser()[3].getAntal());
+        Patient testPatient = Controller.getAllPatienter().get(0);
+        DagligFast testDagligfast = (DagligFast) Controller.getAllPatienter().get(0).getOrdiantioner().get(0);
+        assertSame(dagligFast1,testDagligfast);
+        assertSame(patient,testPatient);
+
 
         //Test 2 slut dato før start dato giver en illegal argument exception
         Exception exception = assertThrows(IllegalArgumentException.class, () -> Controller.opretDagligFastOrdination(LocalDate.of(2023,9,7),
@@ -42,11 +58,24 @@ class ControllerTest {
 
 
         //Test 3 antal enheder på en af tidspunkterne er under nul og et objekt med forkert parameter oprettes
-        DagligFast dagligFast3 = Controller.opretDagligFastOrdination(LocalDate.of(2023,9,7),
-                LocalDate.of(2023,9,12),patient,lægemiddel,
-                -1,1,2,3);
+        double altMorgen = -1;
+        DagligFast dagligFast3 = Controller.opretDagligFastOrdination(start,
+                slut,patient,lægemiddel,
+                altMorgen,middagAntal,aftenAntal,natAntal);
         DagligFast dagligFastTest3 = (DagligFast) Controller.getAllPatienter().get(0).getOrdiantioner().get(1);
         assertEquals(dagligFast3.getDoser()[0].getAntal(), dagligFastTest3.getDoser()[0].getAntal());
+        assertNotNull(dagligFast3);
+        assertSame(lægemiddel,dagligFast3.getLægemiddel());
+        assertSame(start,dagligFast3.getStartDato());
+        assertSame(slut,dagligFast3.getSlutDato());
+        assertEquals(altMorgen,dagligFast3.getDoser()[0].getAntal());
+        assertEquals(middagAntal,dagligFast3.getDoser()[1].getAntal());
+        assertEquals(aftenAntal,dagligFast3.getDoser()[2].getAntal());
+        assertEquals(natAntal,dagligFast3.getDoser()[3].getAntal());
+        Patient testPatient3 = Controller.getAllPatienter().get(0);
+        DagligFast testDagligfast3 = (DagligFast) Controller.getAllPatienter().get(0).getOrdiantioner().get(1);
+        assertSame(dagligFast3,testDagligfast3);
+        assertSame(patient,testPatient3);
 
 
 
@@ -62,6 +91,7 @@ class ControllerTest {
 
     @Test
     void anbefaletDosisPrDøgn() {
+
     }
 
     @Test
