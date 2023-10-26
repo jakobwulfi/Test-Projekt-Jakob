d6090b7d2636b6734ec6978b12f4844a8ee191ce
Jakob Wolf <jakob-wolf14@live.dk>
2023-10-26 09:15:03 +0200

    DagligSkæv fix

---
+++ Test/ordination/DagligSkævTest.java
@@ -0,0 +1,45 @@
+package ordination;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import controller.Controller;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+class DagligSkævTest {
+
+    @Test
+    void samletDosis() {
+        // Laver en ny dagligSkæv Ordination
+        double[] antalEnheder = {3, 2};
+        LocalTime[] tidspunkter = {LocalTime.parse("10:00"), LocalTime.parse("16:00")};
+        DagligSkæv dskæv1 = new DagligSkæv(LocalDate.parse("2019-01-23"), LocalDate.parse("2019-01-24"),
+                tidspunkter, antalEnheder);
+        // Anden testcase
+        double[] antalEnheder2 = {5,2,3};
+        LocalTime[] tidspunkter2 = {LocalTime.parse("12:00"), LocalTime.parse("15:00"), LocalTime.parse("10:00")};
+        DagligSkæv dskæv2 = new DagligSkæv(LocalDate.parse("2019-01-23"), LocalDate.parse("2019-01-24"),
+                tidspunkter2, antalEnheder2);
+        // Assertion
+        double result1 = dskæv1.døgnDosis();
+        double result2 = dskæv2.døgnDosis();
+        assertEquals(5, result1, 0001);
+        assertEquals(10, result2, 0001);
+    }
+
+    @Test
+    void døgnDosis() {
+        // Testcase 1
+        double[] antalEnheder = {3};
+        LocalTime[] tidspunkter = {LocalTime.parse("10:00")};
+        DagligSkæv dskæv1 = new DagligSkæv(LocalDate.parse("2019-01-23"), LocalDate.parse("2019-01-24"),
+                tidspunkter, antalEnheder);
+        // Testcase 2
+        double[] antalEnheder2 = {3};
+        LocalTime[] tidspunkter2 = {LocalTime.parse("10:00")};
+        DagligSkæv dskæv2 = new DagligSkæv(LocalDate.parse("2019-01-20"), LocalDate.parse("2019-01-29"),
+                tidspunkter, antalEnheder);
+    }
+}
