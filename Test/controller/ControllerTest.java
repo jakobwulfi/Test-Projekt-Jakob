de72e05212af347b6d33a8dacaf846bc063855a4
Jakob Wolf <jakob-wolf14@live.dk>
2023-10-26 09:59:30 +0200

    ControllerTest Klasse

---
+++ Test/controller/ControllerTest.java
@@ -0,0 +1,34 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import controller.Controller;
+import org.junit.jupiter.api.*;
+
+class ControllerTest {
+
+    @Test
+    void opretPNOrdination() {
+    }
+
+    @Test
+    void opretDagligFastOrdination() {
+    }
+
+    @Test
+    void opretDagligSkævOrdination() {
+    }
+
+    @Test
+    void anvendOrdinationPN() {
+    }
+
+    @Test
+    void anbefaletDosisPrDøgn() {
+    }
+
+    @Test
+    void antalOrdinationerPrVægtPrLægemiddel() {
+    }
+}
