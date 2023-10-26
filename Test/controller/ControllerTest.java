288c20d7a7069d50991ac59c89618266f7166a64
Jakob Wolf <jakob-wolf14@live.dk>
2023-10-26 11:29:16 +0200

    Tests af opretDagligSkæv() og antalOrdinationerPrVægtPrLægemiddel()

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -1,13 +1,24 @@
 package controller;
 
+import ordination.DagligSkæv;
+import ordination.Lægemiddel;
+import ordination.Patient;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import controller.Controller;
 import org.junit.jupiter.api.*;
+import storage.Storage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 class ControllerTest {
-
+    @BeforeEach
+    public void setUp() {
+        Storage storage = new Storage();
+        Controller.setStorage(storage);
+    }
     @Test
     void opretPNOrdination() {
     }
@@ -18,6 +29,57 @@ class ControllerTest {
 
     @Test
     void opretDagligSkævOrdination() {
+        // Basisdata
+        Patient finn = Controller.opretPatient("070985-1153", "Finn Madsen", 83.2);
+        Lægemiddel fucidin = Controller.opretLægemiddel(
+                "Fucidin", 0.025, 0.025,
+                0.025, "Styk");
+        double[] antalEnheder = {0.5, 1, 2.5, 3};
+        LocalTime[] tidspunkter = {LocalTime.parse("12:00"),LocalTime.parse("12:40"),LocalTime.parse("16:00"),
+                LocalTime.parse("18:45")};
+        LocalDate startdato = LocalDate.parse("2019-01-23");
+        LocalDate slutDato = LocalDate.parse("2019-01-24");
+
+        // TestCase 1 Arrange & Act
+        DagligSkæv tc1 = Controller.opretDagligSkævOrdination(startdato,slutDato,finn,fucidin,tidspunkter,antalEnheder);
+
+        // Assert - checker at objektet bliver oprettet med de rigtige parametre
+        assertNotNull(tc1);
+        assertEquals(finn.getOrdiantioner().get(0), tc1);
+        assertEquals(fucidin, tc1.getLægemiddel());
+        assertEquals(startdato, tc1.getStartDato());
+        assertEquals(slutDato, tc1.getSlutDato());
+        assertArrayEquals(antalEnheder, tc1.getAntalEnheder());
+        assertArrayEquals(tidspunkter,tc1.getKlokkeslet());
+        Patient testPatient = Controller.getAllPatienter().get(0);
+        DagligSkæv tc1Storage = (DagligSkæv) testPatient.getOrdiantioner().get(0);
+        assertEquals(tc1, tc1Storage);
+
+        // Testcase 2
+        LocalDate slutdato2 = startdato;
+        DagligSkæv tc2 = Controller.opretDagligSkævOrdination(startdato,slutdato2,finn,fucidin,tidspunkter,antalEnheder);
+
+        assertNotNull(tc2);
+        assertEquals(finn.getOrdiantioner().get(1), tc2);
+        assertEquals(fucidin, tc2.getLægemiddel());
+
+        // Testcase 3 -> Patient = null, tester for NullPointerException, tester egentlig for en precondition her
+        Exception exception = assertThrows(NullPointerException.class,
+                () -> Controller.opretDagligSkævOrdination(startdato,slutDato,null,fucidin,tidspunkter,antalEnheder));
+
+        // Testcase 4 -> klokkeslet[].length != antalEnheder.length
+        double[] antalEnheder2 = {0.5, 1, 2.5, 3, 7}; // tilføjer et ekstra tal, så arraylængden ikke matcher
+
+        Exception exception2 = assertThrows(IllegalArgumentException.class,
+                () -> Controller.opretDagligSkævOrdination(startdato,slutDato,finn,fucidin,tidspunkter,antalEnheder2));
+        assertEquals("Antal af klokkeslet og enheder matcher ikke.",exception2.getMessage());
+
+        // Testcase 5 -> startDato > slutdato
+        LocalDate startDato2 = LocalDate.parse("2019-01-25");
+
+        Exception exception3 = assertThrows(IllegalArgumentException.class,
+                () -> Controller.opretDagligSkævOrdination(startDato2,slutDato,finn,fucidin,tidspunkter,antalEnheder2));
+        assertEquals("Startdato er efter slutdato.", exception3.getMessage());
     }
 
     @Test
--- Test/ordination/DagligSkævTest.java
+++ Test/ordination/DagligSkævTest.java
@@ -31,34 +31,45 @@ class DagligSkævTest {
 
     @Test
     void samletDosis() {
-        // Testcase 1
+        // Testcase 1 Arrange
         double[] antalEnheder = {3};
         LocalTime[] tidspunkter = {LocalTime.parse("10:00")};
         DagligSkæv dskæv1 = new DagligSkæv(LocalDate.parse("2019-01-23"), LocalDate.parse("2019-01-24"),
                 tidspunkter, antalEnheder);
+        // Act
+        double tc1Result = dskæv1.samletDosis();
+        // Assert
+        assertEquals(6,tc1Result,001);
+
         // Testcase 2
         double[] antalEnheder2 = {3};
         LocalTime[] tidspunkter2 = {LocalTime.parse("10:00")};
         DagligSkæv dskæv2 = new DagligSkæv(LocalDate.parse("2019-01-20"), LocalDate.parse("2019-01-29"),
                 tidspunkter2, antalEnheder2);
+        double tc2Result = dskæv2.samletDosis();
+        assertEquals(30,tc2Result,001);
+
         // Testcase 3
         double[] antalEnheder3 = {5,2,3};
         LocalTime[] tidspunkter3 = {LocalTime.parse("12:00"), LocalTime.parse("15:00"), LocalTime.parse("00:00")};
         DagligSkæv dskæv3 = new DagligSkæv(LocalDate.parse("2019-01-23"), LocalDate.parse("2019-01-23"),
                 tidspunkter3, antalEnheder3);
+        double tc3Result = dskæv3.samletDosis();
+        assertEquals(10,tc3Result,001);
+
         // Testcase 4
         double[] antalEnheder4 = {5,2,3};
         LocalTime[] tidspunkter4 = {LocalTime.parse("12:00"), LocalTime.parse("15:00"), LocalTime.parse("00:00")};
         DagligSkæv dskæv4 = new DagligSkæv(LocalDate.parse("2019-01-20"), LocalDate.parse("2019-01-29"),
                 tidspunkter4, antalEnheder4);
-        // Assertions
-        double tc1Result = dskæv1.samletDosis();;
-        double tc2Result = dskæv2.samletDosis();
-        double tc3Result = dskæv3.samletDosis();
         double tc4Result = dskæv4.samletDosis();
-        assertEquals(6,tc1Result,001);
-        assertEquals(30,tc2Result,001);
-        assertEquals(10,tc3Result,001);
+
+        // Assertions
+
+
+
+
+
         assertEquals(100,tc4Result,001);
     }
 }
